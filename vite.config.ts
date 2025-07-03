10d51357a48afcffee2e358d9942995cabb91e98
Aziz TINDE <117732135+tindeaziz@users.noreply.github.com>
2025-07-03 15:41:40 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,12 +20,6 @@ export default defineConfig({
       }
     }
   },
-});
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173
@@ -38,5 +32,5 @@ export default defineConfig({
       'localhost',
       '127.0.0.1'
     ]
-  }
+  },
 });
